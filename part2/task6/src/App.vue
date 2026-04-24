933c80a85689a6200fe1a6d9d6edcff440a585b0
RedFeuer <danilabespyatov@gmail.com>
2026-04-24 22:18:34 +0300

    ADD: task6

---
+++ part2/task6/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Clicker from './components/Clicker.vue'
+
+const clickers = Array.from({ length: 100 }, (_, index) => ({
+  id: index + 1,
+  amount: index + 1
+}))
+</script>
+
+<template>
+  <main class="page">
+    <h1>Кликеры с prop amount</h1>
+
+    <section class="grid">
+      <Clicker
+        v-for="clicker in clickers"
+        :key="clicker.id"
+        :amount="clicker.amount"
+      />
+    </section>
+  </main>
+</template>
---
+++ part2/task6/src/components/Clicker.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    default: 1
+  }
+})
+
+const points = ref(0)
+
+function increment() {
+  points.value += props.amount
+}
+</script>
+
+<template>
+  <article class="card">
+    <button @click="increment">
+      +{{ amount }}
+    </button>
+
+    <p class="counter">
+      Очки: {{ points }}
+    </p>
+  </article>
+</template>
