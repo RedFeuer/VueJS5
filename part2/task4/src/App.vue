34dc94dfbb48887f9fef0d32fa78937b81b22abf
RedFeuer <danilabespyatov@gmail.com>
2026-04-24 22:14:00 +0300

    ADD: task4

---
+++ part2/task4/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue'
+
+const points = ref(0)
+
+function increment() {
+  points.value += 1
+}
+</script>
+
+<template>
+  <main class="page">
+    <h1>Кликер через Composition API и Single File Component</h1>
+
+    <button @click="increment">
+      Кликнуть
+    </button>
+
+    <p class="counter">
+      Очки: {{ points }}
+    </p>
+  </main>
+</template>
