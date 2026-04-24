adb3b9fefb226391889c64473582dd022d1db7af
RedFeuer <danilabespyatov@gmail.com>
2026-04-24 22:16:57 +0300

    ADD: task5

---
+++ part2/task5/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Clicker from './components/Clicker.vue'
+
+const clickers = Array.from({ length: 100 }, (_, index) => index + 1)
+</script>
+
+<template>
+  <main class="page">
+    <h1>100 независимых кликеров</h1>
+
+    <section class="grid">
+      <Clicker
+        v-for="number in clickers"
+        :key="number"
+      />
+    </section>
+  </main>
+</template>
---
+++ part2/task5/src/components/Clicker.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+const points = ref(0)
+
+function increment() {
+  points.value += 1
+}
+</script>
+
+<template>
+  <article class="card">
+    <button @click="increment">
+      Кликнуть
+    </button>
+
+    <p class="counter">
+      Очки: {{ points }}
+    </p>
+  </article>
+</template>
