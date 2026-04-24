bd67dec53c23b116dbe67a4b1c94829c0ba8420a
RedFeuer <danilabespyatov@gmail.com>
2026-04-24 22:22:02 +0300

    ADD: task7

---
+++ part2/task7/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Clicker from './components/Clicker.vue'
+
+const clickers = Array.from({ length: 100 }, (_, index) => ({
+  id: index + 1,
+  amount: index + 1
+}))
+</script>
+
+<template>
+  <main class="page">
+    <h1>Кликеры с кастомной кнопкой через slot</h1>
+
+    <section class="grid">
+      <Clicker
+        v-for="clicker in clickers"
+        :key="clicker.id"
+        :amount="clicker.amount"
+      >
+        Кликер {{ clicker.id }}: +{{ clicker.amount }}
+      </Clicker>
+    </section>
+  </main>
+</template>
---
+++ part2/task7/src/components/Clicker.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    default: 1
+  }
+})
+
+const points = ref(0)
+
+function increment() {
+  points.value += props.amount
+}
+
+function handleKeydown(event) {
+  if (event.key === 'Enter' || event.key === ' ') {
+    event.preventDefault()
+    increment()
+  }
+}
+</script>
+
+<template>
+  <article class="card">
+    <div
+      class="click-target"
+      role="button"
+      tabindex="0"
+      @click="increment"
+      @keydown="handleKeydown"
+    >
+      <slot>
+        Кликнуть
+      </slot>
+    </div>
+
+    <p class="counter">
+      Очки: {{ points }}
+    </p>
+  </article>
+</template>
